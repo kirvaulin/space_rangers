9bfe50ee0bd7e7e5d0bb7c71aedf9a7203d47823
Vaulin Kirill <kavaulin@miem.hse.ru>
2021-12-17 14:21:26 +0500

    сделал requirements

--- tests.py
+++ tests.py
@@ -1,1 +1,20 @@
-import pytest
+import game
+
+import unittest
+from unittest.mock import patch
+import pygame
+
+
+
+class GameTests(unittest.TestCase):
+
+    @patch('game.stop_game')
+    def test_game_start(self, test_patch):
+        """проверяем работает ли остановка игры при health = 0"""
+        test_patch.return_value = 'win_test'
+        game.health2 = 0
+        self.assertEqual(game.run_game(), 'win_test')
+
+if __name__ == '__main__':
+    unittest.main
+
